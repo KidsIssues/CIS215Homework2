63201df40d58f2f2bc887006d04d2b3e713a39cd
Luke Boardman <lukejamesboardman@icloud.com>
2025-02-07 22:14:43 -0500

    in case code goes boom, might have to redo. i dont really know if this will work since the debug says $Greeting doesnt exist

--- homework2.php
+++ homework2.php
@@ -1,4 +1,15 @@
 <?php
+date_default_timezone_set('UTC');
+
+$CurrentHour = date('H');
+
+if ($CurrentHour <= 10) {
+    $Greeting = "Good Morning. Its a while before Noon.";
+    } elseif ($CurrentHour <= ) {
+        # code...
+    }
+
+
 
 ?>
 
@@ -9,5 +20,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <h1><?php echo $Greeting; </h1>
-    
+    <h1> <?php echo $Greeting ?> </h1>
+</body>
+</html>
